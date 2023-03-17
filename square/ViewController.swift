22a0e98a79399fab7ac78294277ed562c02e228c
maxxxondagon <maxxondagon@gmail.com>
2023-03-17 18:01:38 +0500

    123

--- square/ViewController.swift
+++ square/ViewController.swift
@@ -8,12 +8,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    lazy var animator = UIDynamicAnimator()
+    
+    lazy var square: UIView = {
+        let square = UIView()
+        square.backgroundColor = .systemBlue
+        square.layer.cornerRadius = 8
+        square.translatesAutoresizingMaskIntoConstraints = false
+        return square
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        view.addSubview(square)
+        square.frame = CGRect(x: 0, y:0, width: 100, height: 100)
+        square.center = view.center
     }
-
-
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        let location = touch.location(in: view)
+        createAnimation(point: location)
+    }
+    
+    func createAnimation(point: CGPoint) {
+        animator = UIDynamicAnimator(referenceView: view)
+        let collision = UICollisionBehavior(items: [square])
+        collision.translatesReferenceBoundsIntoBoundary = true
+        animator.addBehavior(collision)
+        
+        let snapBehavior = UISnapBehavior(item: square, snapTo: point)
+        snapBehavior.damping = 1
+        animator.addBehavior(snapBehavior)
+    }
 }
 
